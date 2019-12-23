72cc852cacb055c81d35cd5bd1f7287a981e04a6
flosacca <flosacca@gmail.com>
2019-12-24 04:48:19 +0800

    Bug fix

--- src/main.cc
+++ src/main.cc
@@ -9,20 +9,26 @@ int main() {
 			auto ol = doc.search(words);
 			int n = ol.size();
 			map<List<Array<int, 3>>>(ol).each_with_index([&] (auto& a, int i) {
-				fprintf(f, "(%d,%d)%c", a[2], a[1], i < n-1 ? ' ' : '\n');
+				fprintf(f, "(%d,%d)", a[2], a[1]);
+				if (i < n-1)
+					fputs(" ", f);
 			});
+			fputs("\n", f);
 		});
 	});
 
 	open("result2.txt", "w", [&] (auto& f) {
 		read("query2.txt").split('\n').each([&] (auto& title) {
-			auto vec = doc.recommend(title, 5);
-			if (vec.empty()) {
+			auto ids = doc.recommend(title, 5);
+			if (ids.empty()) {
 				fputs(f, "Movie not found");
 			} else {
-				vec.each_with_index([&] (int id, int i) {
-					fprintf(f, "(%d,%s)%s", id, doc.title(id).data(), i < vec.size()-1 ? ", " : "\n");
+				ids.each_with_index([&] (int id, int i) {
+					fprintf(f, "(%d,%s)", id, doc.title(id).data());
+					if (i < ids.size()-1)
+						fputs(", ", f);
 				});
+				fputs("\n", f);
 			}
 		});
 	});
